a2ec7f2a853eb5859ff81fb234c4ad3dcb71a3bd
davidmitesh <excelrock_mitesh@yahoo.com>
2018-05-14 21:34:29 +0545

    emit message to all tab connected

--- server/server.js
+++ server/server.js
@@ -21,12 +21,13 @@ io.on('connection',(socket)=>{
   socket.on('disconnect',()=>{
     console.log("disconnected from client");
   });
-  socket.emit('newMessageEvent',{
-    from:"excelrock_mitesh@yahoo.com",
-    text:"hey how are u"
-  });
+
   socket.on('createMessageEvent',(message)=>{
     console.log("new message is ",message.text);
+    io.emit('newMessageEvent',{
+      from: message.from,
+      text:message.text
+    });
   });
 });
 
